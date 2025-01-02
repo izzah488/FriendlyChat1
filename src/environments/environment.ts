9c8354eaae27de72bc5a5fd696f81d4667e16a7f
izzah488 <izzahaina71@gmail.com>
2025-01-02 21:50:13 +0800

    update firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
+    apiKey: "AIzaSyA5UIX4kvApo5u3p_M-ljFpEpLbiyEYUvY",
+    authDomain: "friendlychat-2e2ba.firebaseapp.com",
+    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    projectId: "friendlychat-2e2ba",
+    storageBucket: "friendlychat-2e2ba.firebasestorage.app",
+    messagingSenderId: "327884212767",
+    appId: "1:327884212767:web:2ff38372c9a13cb3c6a32a",
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
---
+++ src/irebase-config.js
@@ -0,0 +1,13 @@
+export const environment = {
+    firebase: {
+      apiKey: "AIzaSyA5UIX4kvApo5u3p_M-ljFpEpLbiyEYUvY",
+      authDomain: "friendlychat-2e2ba.firebaseapp.com",
+      databaseURL: "https://PROJECT_ID.firebaseio.com",
+      projectId: "friendlychat-2e2ba",
+      storageBucket: "friendlychat-2e2ba.firebasestorage.app",
+      messagingSenderId: "327884212767",
+      appId: "1:327884212767:web:2ff38372c9a13cb3c6a32a",
+      measurementId: 'G-MEASUREMENT_ID',
+    },
+  };
+  
